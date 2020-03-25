05122b3783d71ac528ff0acccfb622c1a2c5ad0e
majda107 <majda.trpkos@gmail.com>
2020-03-25 19:26:42 +0100

    Working client and server auth

---
+++ AuthServer/AuthServer/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CORSTest.Controllers
+{
+    public class AuthController: ControllerBase
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        private readonly string _client_id = String.Empty;
+        private readonly string _client_secret = String.Empty;
+        public AuthController(IHttpClientFactory clientFactory, IConfiguration configuration)
+        {
+            this._clientFactory = clientFactory;
+
+            this._client_id = configuration["Secret:ClientID"];
+            this._client_secret = configuration["Secret:ClientSecret"];
+        }
+
+        [HttpGet]
+        [Route("auth/GetToken")]
+        public async Task<IActionResult> GetToken(string code)
+        {
+            var client = this._clientFactory.CreateClient();
+
+
+            var res = await client.GetAsync($"https://github.com/login/oauth/access_token?code={code}&client_id={this._client_id}&client_secret={this._client_secret}");
+
+            var content = await res.Content.ReadAsStringAsync();
+
+            Console.WriteLine(" --- TOKEN: " + content);
+
+            var result = new JsonResult(new { token = content });
+
+            return result;
+        }
+    }
+}
---
+++ AuthServer/AuthServer/Startup.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace CORSTest
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHttpClient();
+            services.AddMvc();
+            services.AddCors(options =>
+            {
+                options.AddPolicy(MyAllowSpecificOrigins, builder => 
+                {
+                    builder.WithOrigins("https://api.github.com").AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseCors(MyAllowSpecificOrigins);
+
+            //app.UseMvcWithDefaultRoute();
+            app.UseEndpoints(endpoints =>
+            {
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
+
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
